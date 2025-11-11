62527eeb6962354e8c4cc1ea3ac049a92f9584de
JuanBarron2001 <barronjuan2001@gmail.com>
2025-11-10 22:47:00 -0600

    lesson 01 robot

--- robot-code/command-based-bot-2025/src/main/java/frc/lesson/lesson01/extra/Lesson01.java
+++ robot-code/command-based-bot-2025/src/main/java/frc/lesson/lesson01/extra/Lesson01.java
@@ -3,6 +3,11 @@ package frc.lesson.lesson01.extra;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lesson.LessonBase;
 
+// TASK: Add three SmartDashboard prints you would expect to see during a match.
+// Think like a driver or developer:
+// - What information would help you make quick decisions?
+// - What values would help you debug if something goes wrong?
+
 public class Lesson01 extends LessonBase {
     @Override
     public void execute() {
