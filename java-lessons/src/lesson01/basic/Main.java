1e54e42bd8914f7540bb233b852867261628cf7f
JuanBarron2001 <barronjuan2001@gmail.com>
2025-11-10 22:42:37 -0600

    lesson 01 java

--- java-lessons/src/lesson01/basic/Main.java
+++ java-lessons/src/lesson01/basic/Main.java
@@ -1,6 +1,8 @@
 package lesson01.basic;
 import lessonRunner.BaseLesson;
 
+// TASK: Write your first print statement here.
+// Example: System.out.println("Hello, world!");
 public class Main extends BaseLesson {
     public static void main(String[] args) {
     }
